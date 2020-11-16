f91dc6f7e6962c3969c99a059e0ad91151967f14
scrumpygeo <scrumpygeo@yahoo.ca>
2020-11-16 11:43:20 +0000

    set up db

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const speakeasy = require('speakeasy')
+const uuid = require('uuid')
+const { JsonDB } = require('node-json-db')
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
 
 const app = express()
+
+// initialize db
+const db = new JsonDB(new Config('myDatabase', true, false, '/'))
 
 app.get('/api', (req, res) =>
   res.json({ message: 'This is the 2-factor authentication eg.' })
