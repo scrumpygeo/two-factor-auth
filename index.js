ce16f7616f97450d077c56c4a39beb5042852489
scrumpygeo <scrumpygeo@yahoo.ca>
2020-11-16 11:53:03 +0000

    generate secret

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const app = express()
 // initialize db
 const db = new JsonDB(new Config('myDatabase', true, false, '/'))
 
+// Register user & create temp secret (unam & pwd would go here too)
+app.post('/api/register', (req, res) => {
+  const id = uuid.v4()
+
+  try {
+    const path = `/user/${id}`
+    const temp_secret = speakeasy.generateSecret()
+
+    db.push(path, { id, temp_secret })
+    res.json({ id, secret: temp_secret })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Error generating the secret' })
+  }
+})
+
 app.get('/api', (req, res) =>
   res.json({ message: 'This is the 2-factor authentication eg.' })
 )
